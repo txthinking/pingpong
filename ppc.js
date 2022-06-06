be1d7c92dd2a7bde7e32e8062a6ae3ad3d5e84d6
TxThinking <cloud@txthinking.com>
2022-06-06 18:12:55 +0800

    Update ppc.js

--- ppc.js
+++ ppc.js
@@ -31,9 +31,9 @@ if (args.c) {
 }
 
 var data = new Uint8Array([231, 155, 184, 230, 175, 148, 228, 186, 142, 65, 110, 100, 114, 111, 105, 100, 239, 188, 140, 229, 188, 128, 229, 143, 145, 105, 79, 83, 229, 186, 148, 231, 148, 168, 229, 174, 161, 230, 160, 184, 231, 156, 159, 230, 152, 175, 229, 138, 179, 229, 191, 131, 232, 180, 185, 231, 165, 158, 239, 188, 140, 229, 191, 131, 231, 180, 175, 239, 188, 129]);
+var b = new Uint8Array(23);
 
 var conn = await Deno.connect({ hostname: splithostport(args.s)[0], port: splithostport(args.s)[1], transport: "tcp" });
-var b = new Uint8Array(23);
 for (var j = 0; j < n; j++) {
     var i = await conn.write(data);
     echo(`tcp src: ${joinhostport(conn.localAddr.hostname, conn.localAddr.port)} dst: ${joinhostport(conn.remoteAddr.hostname, conn.remoteAddr.port)} data: ${b2s(data)}`);
@@ -57,7 +57,6 @@ for (var p = 7777; true; p++) {
 for (var i = 0; i < n; i++) {
     await c.send(data, { transport: "udp", hostname: splithostport(args.s)[0], port: parseInt(splithostport(args.s)[1]) });
     echo(`udp src: ${joinhostport(c.addr.hostname, c.addr.port)} dst: ${args.s} data: ${b2s(data)}`);
-    var b = new Uint8Array(23);
     var l = await c.receive(b);
     echo(`udp src: ${joinhostport(l[1].hostname, l[1].port)} dst: ${joinhostport(c.addr.hostname, c.addr.port)} data: ${b2s(l[0])}`);
 }
